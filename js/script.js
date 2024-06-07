2a03a85db03510e3ba3935f78162c105d3736939
Yuvaraj0868 <yuvaraj0868@gmail.com>
2024-06-07 17:28:15 +0530

    updatedProject

--- js/script.js
+++ js/script.js
@@ -19,11 +19,35 @@
     let darkmode = document.querySelector('#darkmode');
      
     darkmode.onclick = () => {
-        if(darkmode.classList.contains('bx-moon')){
-            darkmode.classList.replace('bx-moon','bx-sun');
+        if(darkmode.classList.contains('bx-sun')){
+            darkmode.classList.replace('bx-sun','bx-moon');
             document.body.classList.add('active');
         }else{
-            darkmode.classList.replace('bx-sun','bx-moon');
+            darkmode.classList.replace('bx-moon','bx-sun');
             document.body.classList.remove('active');
         }
     }
+
+    // const contactForm = document.getElementById("contact-form"),
+    //       contactMessage = document.getElementById("contact-message")
+
+
+    // const sendEmail = (e)=>{
+    //     e.preventDefault()
+
+    //     // serviceID - templeteID - #form - publickey
+
+    //     emailjs.sendForm('service_a9ooeo9','template_0sw32bg','#contact-form','w85yGTXpb-Z_4mGlX')
+    //         .then(()=>{
+    //             // Show Sent Message
+    //             contactMessage.textContent = 'Message sent successfully !!!'
+
+    //             // Remove Message
+    //             setTimeout(()=>{
+    //             contactMessage.textContent = ''
+    //             },5000)
+    //         })
+
+    // }
+
+    // contactForm.addEventListener('click',sendEmail)
